751744935536c060671e01c018bd9cd7ac5e2706
Igor Borba <142315640+BorbaaDev@users.noreply.github.com>
2023-10-02 20:02:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import os
 
 
@@ -19,37 +18,39 @@ print('='*45)
 print(' + - * /   CALCULADORA DO BORBA      + - * /       ')
 print('='*45)
 
-print(' Escolha o sinal da operação que deseja realizar o cálculo\n ou Digite sair para encerrar. ')
+print(' Escolha o sinal da operação que deseja realizar o cálculo ou Digite sair para encerrar. ')
 print('-'*25)
 operadores = str(input(' SOMA(+)\n SUBTRAÇÃO(-)\n DIVISÃO(/)\n MULTIPLICAÇÃO(*)\n'))
 os.system("clear")
 while operadores != 'sair':
-    n1 = float(input('Digite o 1º valor: '))
-    print('-'*25)
-    n2 = float(input('Digite o 2º valor: '))
-    print('-'*25)
-
-    if operadores == '+':
-        print('RESULTADO')
-        print(soma (n1, n2))
-
-    elif operadores == '-':
-        print('RESULTADO')
-        print(subtracao (n1, n2))
-
-    elif operadores == '*':
-        print('RESULTADO')
-        print(multiplica (n1, n2))
-
-    elif operadores == '/':
-        print('RESULTADO')
-        print(divisao (n1, n2))
-
+    if operadores != '+' and operadores != '-' and operadores != '/' and operadores != '*':
+       print('OPERAÇÃO INVÁLIDA!')
+       print('Digite a  operação correta, MACHO !')
     else:
-      print(' ')
-      print(' ')
-      print('-' *25)
-    print(' Escolha o sinal da operação que deseja realizar o cálculo\n ou Digite sair para encerrar. ')
+        n1 = float(input('Digite o 1º valor: '))
+        print('-'*25)
+        n2 = float(input('Digite o 2º valor: '))
+        print('-'*25)
+
+        if operadores == '+':
+            print('RESULTADO')
+            print(soma (n1, n2))
+
+        elif operadores == '-':
+            print('RESULTADO')
+            print(subtracao (n1, n2))
+
+        elif operadores == '*':
+            print('RESULTADO')
+            print(multiplica (n1, n2))
+
+        elif operadores == '/':
+            print('RESULTADO')
+            print(divisao (n1, n2))
+
+    print(' ')
+    print('-' *25)
+    print(' Escolha o sinal da operação que deseja realizar o cálculo ou Digite sair para encerrar. ')
     print('-'*25)
     operadores = str(input(' SOMA(+)\n SUBTRAÇÃO(-)\n DIVISÃO(/)\n MULTIPLICAÇÃO(*)\n'))
     os.system("clear")
